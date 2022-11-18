2e037495d4750b5bae7d7545fcb5d47a8209ae95
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-11-18 18:28:40 -0300

    feat(class Monster): create class Monster, req 9

---
+++ src/Monster.ts
@@ -0,0 +1,32 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const life = this._lifePoints - attackPoints;
+
+    if (life < 1) {
+      this._lifePoints = -1;
+    }
+    return life;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
